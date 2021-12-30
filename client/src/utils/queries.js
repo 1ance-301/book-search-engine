aabc4c8da8b9ddbd0756169d8a7b962141af6695
Lance Bontrager <87950314+1ance-301@users.noreply.github.com>
2021-12-30 14:27:17 -0500

    queries and mutations

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,52 +1,6 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_BOOKS = gql`
-  query books($title: String) {
-    books(title: $username) {
-      bookId
-      authors
-      description
-      title
-      image
-      link
-      }
-    }
-  }
-`;
-
-export const QUERY_BOOK = gql`
-  query book($id: ID!) {
-    book(_id: $id) {
-      bookId
-      authors
-      description
-      title
-      image
-      link
-    }
-  }
-`;
-
-export const QUERY_USER = gql`
-  query user($username: String!) {
-    user(username: $username) {
-      _id
-      username
-      email
-      bookCount
-      savedBooks {
-        bookId
-        authors
-        description
-        title
-        image
-        link
-      }
-    }
-  }
-`;
-
-export const QUERY_ME = gql`
+export const GET_ME = gql`
   {
     me {
       _id
@@ -60,20 +14,6 @@ export const QUERY_ME = gql`
         title
         image
         link
-    }
-  }
-`;
-
-export const QUERY_ME_BASIC = gql`
-  {
-    me {
-      _id
-      username
-      email
-      bookCount
-      savedBooks {
-        bookId
-        title
     }
   }
 `;
